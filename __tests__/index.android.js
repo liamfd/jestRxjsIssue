184d0fe30ee1edb0e2ee687294032cb0add0951a
Liam Doran <liam@minibardelivery.com>
2017-01-08 17:49:33 -0500

    some notes

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,13 +1,36 @@
-import 'react-native';
-import rx from 'rxjs';
-import React from 'react';
-import Index from '../index.android.js';
+// NOT ADDING RAF TO GLOBAL:
+// import rx from 'rxjs';
+// import React from 'react';
+// import Index from '../index.android.js';
+// import renderer from 'react-test-renderer';
+// var requestAnimationFrame = require('fbjs/lib/requestAnimationFrame');
+// var thing = require('react-native/Libraries/Animated/src/AnimatedImplementation');
+// var requestAnimationFrame = require('fbjs');
+// var thing = require('fbjs/lib/nativeRequestAnimationFrame');
+// var thing = require('react-native/ReactAndroid');
+
+// CONTAINS THE CULPRIT, DOES NOT ADD RAF TO GLOBAL
+// import thing from 'react-native/Libraries/Core/Timers/JSTimers'; // <- defines it @ 108
+
+// ADDING RAF TO GLOBAL
+// import 'react-native';
+import thing from 'react-native/Libraries/Core/initializeCore'; // <- adds to global @ 137
+// it tries to do the same with cAF, but doesn't seem to be working
+
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+  // console.log('raf:', global.requestAnimationFrame)
+  // console.log('caf:', global.cancelAnimationFrame)
+  // if (typeof thing !== 'undefined') console.log('thing:', thing);
+
+  // console.log(global.requestAnimationFrame());
+  // console.log(global.cancelAnimationFrame());
+
+
+
+  // const tree = renderer.create(
+  //   <Index />
+  // );
 });
