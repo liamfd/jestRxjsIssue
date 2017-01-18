243520a0523f8817564818e06a2796dd46dbfbb9
Liam Doran <liam@minibardelivery.com>
2017-01-18 11:41:43 -0500

    updated with the new lazy

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,30 +1,37 @@
 // grabbed from https://github.com/facebook/react-native/blob/a6844bdf7589b38297328e69f61e2986a129ab30/Libraries/Utilities/defineLazyObjectProperty.js
-function defineLazyObjectProperty(
-  object,
-  name,
-  descriptor,
-){
+function defineLazyObjectProperty<T>(
+  object: Object,
+  name: string,
+  descriptor: {
+    get: () => T,
+    enumerable?: boolean,
+    writable?: boolean,
+  },
+): void {
   const {get} = descriptor;
   const enumerable = descriptor.enumerable !== false;
   const writable = descriptor.writable !== false;
 
   let value;
   let valueSet = false;
-  let counter = 0;
-  function getValue() {
+  function getValue(): T {
     // WORKAROUND: A weird infinite loop occurs where calling `getValue` calls
     // `setValue` which calls `Object.defineProperty` which somehow triggers
     // `getValue` again. Adding `valueSet` breaks this loop.
-
     if (!valueSet) {
+      // Calling `get()` here can trigger an infinite loop if it fails to
+      // remove the getter on the property, which can happen when executing
+      // JS in a V8 context.  `valueSet = true` will break this loop, and
+      // sets the value of the property to undefined, until the code in `get()`
+      // finishes, at which point the property is set to the correct value.
+      valueSet = true;
       setValue(get());
     }
-    return get();
+    return value;
   }
-  function setValue(newValue) {
+  function setValue(newValue: T): void {
     value = newValue;
     valueSet = true;
-
     Object.defineProperty(object, name, {
       value: newValue,
       configurable: true,
@@ -40,7 +47,6 @@ function defineLazyObjectProperty(
     enumerable,
   });
 }
-
 // grabbed from https://github.com/facebook/react-native/blob/4252ac75eaf04aa9d57027e91bf7665717ed4eab/Libraries/Core/InitializeCore.js#L129
 const defineLazyTimer = name => {
   defineLazyObjectProperty(global, name, {
